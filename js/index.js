09916538b1b65539dfdeab35a4d198f7020c15ba
Ishowspeedinsights <181628611+Ishowspeedinsights@users.noreply.github.com>
2024-09-16 08:45:41 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -61,23 +61,23 @@ document.addEventListener('DOMContentLoaded', () => {
             postsContainer.appendChild(postElement);
         });
 
-        // Handle media
-        const imageContent = document.getElementById('image-content');
-        const videoContent = document.getElementById('video-content');
-
-        // Set image content
-        if (mediaData.imageOfTheDay) {
-            imageContent.innerHTML = mediaData.imageOfTheDay.htmlTag;
-            document.getElementById('image-date').textContent = mediaData.imageOfTheDay.date;
-            document.getElementById('image-description').textContent = mediaData.imageOfTheDay.description;
-        }
-
-        // Set video content
-        if (mediaData.videoOfTheDay) {
-            videoContent.innerHTML = mediaData.videoOfTheDay.htmlTag;
-            document.getElementById('video-date').textContent = mediaData.videoOfTheDay.date;
-            document.getElementById('video-description').textContent = mediaData.videoOfTheDay.description;
-        }
+//        // Handle media
+//        const imageContent = document.getElementById('image-content');
+//        const videoContent = document.getElementById('video-content');
+//
+//        // Set image content
+//        if (mediaData.imageOfTheDay) {
+//            imageContent.innerHTML = mediaData.imageOfTheDay.htmlTag;
+//            document.getElementById('image-date').textContent = mediaData.imageOfTheDay.date;
+//            document.getElementById('image-description').textContent = mediaData.imageOfTheDay.description;
+//        }
+//
+//        // Set video content
+//        if (mediaData.videoOfTheDay) {
+//            videoContent.innerHTML = mediaData.videoOfTheDay.htmlTag;
+//            document.getElementById('video-date').textContent = mediaData.videoOfTheDay.date;
+//            document.getElementById('video-description').textContent = mediaData.videoOfTheDay.description;
+//        }
     })
     .catch(error => console.error('Error fetching data:', error));
 });
